92c0ee7fea9405471ce9cabda5e47e44144d7028
Ami Zou <ami.yx.zou@gmail.com>
2018-01-20 14:00:35 +0000

    Initial Commit

---
+++ HackCambridge2018/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HackCambridge2018
+//
+//  Created by Ami Zou on 1/20/18.
+//  Copyright © 2018 Gamma Lab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HackCambridge2018/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HackCambridge2018
+//
+//  Created by Ami Zou on 1/20/18.
+//  Copyright © 2018 Gamma Lab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
