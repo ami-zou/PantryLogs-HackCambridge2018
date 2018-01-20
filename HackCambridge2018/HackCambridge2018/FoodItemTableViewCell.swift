6cec99fd840d7006e083f1ce00f265c87181186c
Ami Zou <ami.yx.zou@gmail.com>
2018-01-20 17:05:15 +0000

    table cell

---
+++ HackCambridge2018/HackCambridge2018/FoodItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodItemTableViewCell.swift
+//  HackCambridge2018
+//
+//  Created by Ami Zou on 1/20/18.
+//  Copyright © 2018 Women Hackers. All rights reserved.
+//
+
+import UIKit
+
+class FoodItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
