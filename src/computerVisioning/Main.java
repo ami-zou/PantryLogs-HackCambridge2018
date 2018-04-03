723de9e3b00f9d29a59345728e4aa08402f8a53c
Ami Zou <ami.yx.zou@gmail.com>
2018-04-03 14:56:44 +0100

    Import Maven libraries

--- src/computerVisioning/Main.java
+++ src/computerVisioning/Main.java
@@ -1,4 +1,14 @@
 package computerVisioning;
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
 public class Main {
 
