5c634ff1691d85af7b1560df7492d571fe3db238
Ami Zou <ami.yx.zou@gmail.com>
2018-04-03 15:10:45 +0100

    OCR Java Sample

--- src/computerVisioning/OCR.java
+++ src/computerVisioning/OCR.java
@@ -1,5 +1,68 @@
 package computerVisioning;
+//This sample uses the Apache HTTP client library(org.apache.httpcomponents:httpclient:4.2.4)
+//and the org.json library (org.json:json:20170516).
 
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+//Code from https://docs.microsoft.com/en-us/azure/cognitive-services/Computer-vision/QuickStarts/Java
 public class OCR {
+	public static final String subscriptionKey = "3c948fecbdf64ac4957ab87e575c4fd5";
+	public static final String region = "unitedstates";
+	public static final String uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr";
+	//Similar to CS, just change "/analyze" to "/ocr"
+	
+	public static void main(String[] args) {
+		HttpClient httpClient = new DefaultHttpClient();
 
+        try
+        {
+            // NOTE: You must use the same location in your REST call as you used to obtain your subscription keys.
+            //   For example, if you obtained your subscription keys from westus, replace "westcentralus" in the
+            //   URL below with "westus".
+            URIBuilder uriBuilder = new URIBuilder(uriBase);
+
+            uriBuilder.setParameter("language", "unk");
+            uriBuilder.setParameter("detectOrientation ", "true");
+
+            // Request parameters.
+            URI uri = uriBuilder.build();
+            HttpPost request = new HttpPost(uri);
+
+            // Request headers.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request body.
+            StringEntity requestEntity =
+                    new StringEntity("{\"url\":\"https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Atomist_quote_from_Democritus.png/338px-Atomist_quote_from_Democritus.png\"}");
+            request.setEntity(requestEntity);
+
+            // Execute the REST API call and get the response entity.
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null)
+            {
+                // Format and display the JSON response.
+                String jsonString = EntityUtils.toString(entity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("REST Response:\n");
+                System.out.println(json.toString(2));
+            }
+        }
+        catch (Exception e)
+        {
+            // Display error message.
+            System.out.println(e.getMessage());
+        }
+	}
 }
